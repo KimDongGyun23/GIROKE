1b43637938a3f03c36922594c49822d6dd1881cc
DongGyun <wooheuy5752@gmail.com>
2024-12-20 14:26:39 +0900

    chore: 레이아웃 설정 (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-function App() {
+const App = () => {
   return (
-    <>
-      <h1 className="text-green-600">app</h1>
-      <div></div>
-    </>
+    <div className="flex-center">
+      <div className="scroll relative h-svh w-full min-w-[320px] max-w-[450px] border-x">
+        <h1>app</h1>
+      </div>
+    </div>
   )
 }
 
