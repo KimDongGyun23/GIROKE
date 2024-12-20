51026f0dc5ff5ff96fb449e5304c2637c4a1c482
DongGyun <wooheuy5752@gmail.com>
2024-12-20 14:16:23 +0900

    chore : tailwind 설정 (#1)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,55 @@
-/** @type {import('tailwindcss').Config} */
+import scrollbarHide from 'tailwind-scrollbar-hide'
+
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    fontWeight: {
+      regular: 400,
+      medium: 500,
+      bold: 700,
+    },
+    boxShadow: {
+      shadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25), 0px -2px 4px 0px rgba(0, 0, 0, 0.15)',
+    },
+    extend: {
+      colors: {
+        green: {
+          1: '#DCF8DB',
+          2: '#BEF0BC',
+          3: '#9DE49B',
+          4: '#72D270',
+          5: '#58C85A',
+          6: '#1EB65B',
+          7: '#12A054',
+        },
+        grey: {
+          1: '#FBFBFB',
+          2: '#F2F2F2',
+          3: '#E2E2E2',
+          4: '#BABABA',
+          5: '#9E9E9E',
+          6: '#777777',
+          7: '#3C3C3C',
+        },
+        yellow: {
+          1: '#FFFCE4',
+          2: '#FFF6A0',
+          3: '#FFDF56',
+          4: '#FFCE00',
+          5: '#FDB714',
+          6: '#FC9712',
+          7: '#F47806',
+        },
+        success: '#9CDE70',
+        error: '#FF3B3B',
+        dimmed: 'rgba(34, 40, 48, 0.60)',
+      },
+      utilities: {
+        '.webkit-overflow-scrolling-touch': {
+          '-webkit-overflow-scrolling': 'touch',
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [scrollbarHide],
 }
