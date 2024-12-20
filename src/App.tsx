3548d71303951121ea179dcfa6092f6ce653b6fc
DongGyun <wooheuy5752@gmail.com>
2024-12-20 15:23:58 +0900

    feat: Icon 컴포넌트 구현 (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { CheckBoxIcon } from './components/icons/ActiveIcon'
+
 const App = () => {
   return (
     <div className="flex-center">
       <div className="scroll relative h-svh w-full min-w-[320px] max-w-[450px] border-x">
         <h1>app</h1>
+        <CheckBoxIcon active />
+        <CheckBoxIcon />
       </div>
     </div>
   )
---
+++ src/types/common.ts
@@ -0,0 +1,3 @@
+export type ActiveIconProps = {
+  active?: boolean
+}
