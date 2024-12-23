4e69238e0898e472aed68bea3d5637512a40c235
DongGyun <wooheuy5752@gmail.com>
2024-12-23 17:34:45 +0900

    feat: HomeEdit 페이지 구현 (#3)

--- src/components/view/Router.tsx
+++ src/components/view/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 
 import { Home } from '../container/Home'
 import { HomeCreate } from '../container/HomeCreate'
+import { HomeEdit } from '../container/HomeEdit'
 
 export const RouterComponent = () => {
   return (
@@ -9,6 +10,7 @@ export const RouterComponent = () => {
       <Route path="/" element={<div />} />
       <Route path="/home" element={<Home />} />
       <Route path="/home/create" element={<HomeCreate />} />
+      <Route path="/home/edit" element={<HomeEdit />} />
 
       <Route path="/project" element={<div />} />
       <Route path="/note" element={<div />} />
