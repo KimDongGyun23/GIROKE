e7c7280ab1cdfcce518bb6047aff7548c2d53b6a
DongGyun <wooheuy5752@gmail.com>
2024-12-26 16:01:10 +0900

    feat: Loading 컴포넌트 구현 (#11)

---
+++ src/components/view/Loading.tsx
@@ -0,0 +1,9 @@
+import { SyncLoader } from 'react-spinners'
+
+export const Loading = () => {
+  return (
+    <div className="flex-center size-full">
+      <SyncLoader color="#58C85A" />
+    </div>
+  )
+}
