f4cc1a3e0cfa4c81a3bf1735c8e641ef9f272451
DongGyun <wooheuy5752@gmail.com>
2025-01-10 23:41:03 +0900

    refactor: SideNavbar 로직 변경
    url로 이동하기에 상태 변경이 굳이 필요하지 않아 제거함

--- src/components/view/SideNavbar.tsx
+++ src/components/view/SideNavbar.tsx
@@ -1,46 +1,40 @@
-import { useState } from 'react'
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
 import { NavHomeIcon, NavNoteIcon, NavRecordIcon, NavTodoIcon } from './icons/NavIcon'
 
 const NAV_LIST = [
-  { label: '홈', Icon: NavHomeIcon },
-  { label: '기록', Icon: NavRecordIcon },
-  { label: '할일', Icon: NavTodoIcon },
-  { label: '노트', Icon: NavNoteIcon },
+  { label: '홈', Icon: NavHomeIcon, url: '/' },
+  { label: '기록', Icon: NavRecordIcon, url: '/record' },
+  { label: '할일', Icon: NavTodoIcon, url: '/todo' },
+  { label: '노트', Icon: NavNoteIcon, url: '/note' },
 ] as const
 
 type NavItemProps = {
   item: (typeof NAV_LIST)[number]
   isActive: boolean
-  onClick: VoidFunction
 }
 
-const NavItem = ({ item, isActive, onClick }: NavItemProps) => {
+const NavItem = React.memo(({ item, isActive }: NavItemProps) => {
   const itemStyle = isActive ? 'bg-black-100 text-black-600' : 'bg-black-600 text-black-100'
 
   return (
-    <button className={`flex gap-4 rounded-xl px-4 py-3 ${itemStyle}`} onClick={onClick}>
+    <Link to={item.url} className={`flex gap-4 rounded-xl px-4 py-3 ${itemStyle}`}>
       <div>
         <item.Icon active={isActive} />
       </div>
       <span className="translate-y-[1px] text-xl">{item.label}</span>
-    </button>
+    </Link>
   )
-}
+})
 
 export const SideNavbar = () => {
-  const [activeTab, setActiveTab] = useState(0)
-  const handleChangeTab = (tab: number) => setActiveTab(tab)
+  const { pathname } = useLocation()
 
   return (
-    <nav className="flex-column col-span-1 h-full justify-center gap-4 rounded-xl bg-black-600 p-4 drop-shadow-sm">
-      {NAV_LIST.map((item, index) => (
-        <NavItem
-          key={index}
-          item={item}
-          isActive={activeTab === index}
-          onClick={() => handleChangeTab(index)}
-        />
+    <nav className="col-span-1 flex h-full flex-col justify-center gap-4 rounded-xl bg-black-600 p-4 drop-shadow-sm">
+      {NAV_LIST.map((item) => (
+        <NavItem key={item.url} item={item} isActive={item.url === pathname} />
       ))}
     </nav>
   )
